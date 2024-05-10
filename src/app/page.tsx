7cfaedb2133392e0d00912984980725e28d85e87
Filipe Moreno <filipe.moreno@educacional.local>
2024-05-10 12:11:46 -0300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,421 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 
+import Link from "next/link";
+import { useState } from "react";
+
+interface JogosProps {
+	id: number;
+	numeroJogo: number;
+	data: string;
+	hora: string;
+	periodo: string;
+	modalidade: string;
+	equipeA: string;
+	representanteA: string;
+	equipeB: string;
+	representanteB: string;
+	placarA?: number | null;
+	placarB?: number | null;
+	status: string;
+	finalizado: boolean;
+}
+
+function BellIcon(props) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+			<path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+		</svg>
+	);
+}
+
+function CalendarIcon(props) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M8 2v4" />
+			<path d="M16 2v4" />
+			<rect width="18" height="18" x="3" y="4" rx="2" />
+			<path d="M3 10h18" />
+		</svg>
+	);
+}
+
+function NewspaperIcon(props) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
+			<path d="M18 14h-8" />
+			<path d="M15 18h-5" />
+			<path d="M10 6h8v4h-8V6Z" />
+		</svg>
+	);
+}
+
+function TrophyIcon(props) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
+			<path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
+			<path d="M4 22h16" />
+			<path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
+			<path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
+			<path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
+		</svg>
+	);
+}
+
+function UsersIcon(props) {
+	return (
+		<svg
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+			<circle cx="9" cy="7" r="4" />
+			<path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+			<path d="M16 3.13a4 4 0 0 1 0 7.75" />
+		</svg>
+	);
+}
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [jogos, setJogos] = useState<JogosProps[]>([
+		{
+			id: 1,
+			numeroJogo: 1,
+			modalidade: "Futsal Feminino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "6º ANO A",
+			representanteA: "Natália",
+			equipeB: "6º ANO E",
+			representanteB: "Analú",
+			placarA: 0,
+			placarB: 1,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 2,
+			numeroJogo: 1,
+			modalidade: "Futsal Masculino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "6º ANO A",
+			representanteA: "Gustavo",
+			equipeB: "6º ANO C",
+			representanteB: "Gabriel",
+			placarA: 3,
+			placarB: 2,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 3,
+			numeroJogo: 8,
+			modalidade: "Futsal Feminino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "8º ANO A",
+			representanteA: "Myra",
+			equipeB: "8º ANO C",
+			representanteB: "Mônica",
+			placarA: 0,
+			placarB: 4,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 4,
+			numeroJogo: 15,
+			modalidade: "Futsal Masculino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "3ª SÉRIE A",
+			representanteA: "Mateus",
+			equipeB: "1ª SÉRIE A",
+			representanteB: "Nathan",
+			placarA: 3,
+			placarB: 6,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 5,
+			numeroJogo: 14,
+			modalidade: "Futsal Feminino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "1ª SÉRIE A",
+			representanteA: "Geovana",
+			equipeB: "1ª SÉRIE B",
+			representanteB: "Mariana",
+			placarA: 1,
+			placarB: 0,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 6,
+			numeroJogo: 16,
+			modalidade: "Futsal Masculino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "2ª SÉRIE A",
+			representanteA: "Andre",
+			equipeB: "2ª SÉRIE B",
+			representanteB: "Lucas",
+			placarA: 2,
+			placarB: 3,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 7,
+			numeroJogo: 6,
+			modalidade: "Futsal Masculino",
+			data: "04/07/2022",
+			hora: "07:30",
+			periodo: "Manhã",
+			equipeA: "7º ANO A",
+			representanteA: "Enzo",
+			equipeB: "7º ANO D",
+			representanteB: "Nicolas",
+			placarA: 3,
+			placarB: 1,
+			status: "Encerrado",
+			finalizado: true,
+		},
+		{
+			id: 8,
+			numeroJogo: 6,
+			modalidade: "Futsal Masculino",
+			data: "04/07/2022",
+			hora: "13:30",
+			periodo: "Tarde",
+			equipeA: "6º ANO B",
+			representanteA: "Miguel",
+			equipeB: "6º ANO D",
+			representanteB: "Vitor",
+			placarA: 2,
+			placarB: 4,
+			status: "Encerrado",
+			finalizado: true,
+		},
+	]);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const jogosManha = jogos.filter((jogo) => jogo.periodo === "Manhã");
+	const jogosTarde = jogos.filter((jogo) => jogo.periodo === "Tarde");
+	return (
+		<div className="grid min-h-screen w-full grid-cols-[280px_1fr]">
+			<div className="hidden border-r bg-secondary lg:block">
+				<div className="flex h-full max-h-screen flex-col gap-2">
+					<div className="flex h-[60px] items-center border-b px-6">
+						<Link
+							className="flex items-center justify-center gap-2 font-semibold"
+							href="#"
+						>
+							<Image
+								src="/logo.png"
+								width={250}
+								height={200}
+								alt="Logo do Colégio Santo Inácio"
+								className="p-4 my-4 flex items-center justify-center"
+							/>
+						</Link>
+					</div>
+					<div className="flex flex-col items-center justify-center text-sm text-zinc-300 font-bold">
+						<span>XXXIX OLIMPÍADA INTERNA</span>
+						<span>2024</span>
+					</div>
+					<div className="flex-1 overflow-auto py-2">
+						<nav className="grid items-start px-4 text-sm font-medium">
+							<Link
+								className="flex items-center gap-3 rounded-lg  px-3 py-2 text-gray-900 transition-all hover:text-gray-900 bg-primary-foreground dark:text-gray-50 dark:hover:text-gray-50"
+								href="#"
+							>
+								<CalendarIcon className="h-4 w-4" />
+								Schedule
+							</Link>
+							<Link
+								className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+								href="#"
+							>
+								<TrophyIcon className="h-4 w-4" />
+								Standings
+							</Link>
+							<Link
+								className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+								href="#"
+							>
+								<UsersIcon className="h-4 w-4" />
+								Teams
+							</Link>
+							<Link
+								className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+								href="#"
+							>
+								<NewspaperIcon className="h-4 w-4" />
+								News
+							</Link>
+						</nav>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col">
+				<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+					<div>
+						<h1 className="text-2xl font-bold">FUTSAL</h1>
+					</div>
+					<div className="flex flex-col gap-4">
+						{jogosManha.map((jogo) => {
+							function determinarVencedor(jogo: JogosProps) {
+								if (jogo.placarA > jogo.placarB) {
+									return "equipeA";
+								} else if (jogo.placarA < jogo.placarB) {
+									return "equipeB";
+								} else {
+									return null; // Empate
+								}
+							}
+							return (
+								<Card key={jogo.id} className="bg-secondary">
+									<CardHeader>
+										<CardTitle>
+											<div className="flex justify-between items-center">
+												<span>Jogo {jogo.numeroJogo}</span>
+												<span>
+													{jogo.data} às {jogo.hora}
+												</span>
+											</div>
+										</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<div
+											className={`flex flex-col gap-2 ${
+												jogo.finalizado ? "font-normal" : "font-bold"
+											}`}
+										>
+											<div
+												className={`flex justify-between ${
+													determinarVencedor(jogo) === "equipeA"
+														? "text-green-500 font-bold"
+														: ""
+												}`}
+											>
+												<span>
+													{jogo.equipeA} ({jogo.representanteA})
+												</span>
+												<span>{jogo.placarA}</span>
+											</div>
+											<div
+												className={`flex justify-between ${
+													determinarVencedor(jogo) === "equipeB"
+														? "text-green-500 font-bold"
+														: ""
+												}`}
+											>
+												<span>
+													{jogo.equipeB} ({jogo.representanteB})
+												</span>
+												<span>{jogo.placarB}</span>
+											</div>
+										</div>
+									</CardContent>
+									<CardFooter className="flex justify-between">
+										<Badge
+											className={
+												jogo.finalizado ? "bg-green-500" : "bg-yellow-500"
+											}
+										>
+											{jogo.status}
+										</Badge>
+										<div>
+											<Badge
+												className={
+													(jogo.modalidade === "Futsal Feminino" &&
+														"bg-pink-400") ||
+													"bg-blue-400"
+												}
+											>
+												{jogo.modalidade}
+											</Badge>
+										</div>
+									</CardFooter>
+								</Card>
+							);
+						})}
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
